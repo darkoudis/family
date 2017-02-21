2b578497bcada9fffab82581f4ff82206d4d883d
a.darkoudis <a.darkoudis@gmail.com>
2017-02-21 03:19:26 +0200

    initial commit

---
+++ app/src/main/java/gr/mrp/family/MainActivity.java
@@ -0,0 +1,18 @@
+package gr.mrp.family;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    Button btnExpences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
