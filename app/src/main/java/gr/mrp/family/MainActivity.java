adce8f30316ca6f9fc51a6ecb5892983c2e746d6
a.darkoudis <a.darkoudis@gmail.com>
2017-02-21 08:06:58 +0200

    button works

--- app/src/main/java/gr/mrp/family/MainActivity.java
+++ app/src/main/java/gr/mrp/family/MainActivity.java
@@ -2,7 +2,9 @@ package gr.mrp.family;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     Button btnExpences;
@@ -11,8 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnExpences = (Button) findViewById(R.id.btnExpences);
+        btnExpences.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Expenses click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
-
 
 }
